e25f6df690561c5350dc0de685f4fd57f30b9b3c
karmayoga <karmayoga8149@gmail.com>
2022-08-02 08:44:22 +0800

    add blade

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/suratlampiran', function () {
+    return view('lampiran',[
+        'station' => 'THT',
+        'group' => 'Grup 1',
+        'student' => [
+            ['name'=> 'student 1', 'nim' => '1234'],
+            ['name'=> 'student 2', 'nim' => '3445'],
+            ['name'=> 'student 3', 'nim' => '4556'],
+            ['name'=> 'student 4', 'nim' => '5678'],
+        ]
+    ]);
+});
+
+Route::get('/sendresidence', function () {
+    return view('SendResidence',[
+        'station' => 'THT',
+        'group' => 'Grup 1',
+        'student' => [
+            ['name'=> 'student 1', 'nim' => '1234'],
+            ['name'=> 'student 2', 'nim' => '3445'],
+            ['name'=> 'student 3', 'nim' => '4556'],
+            ['name'=> 'student 4', 'nim' => '5678'],
+        ]
+    ]);
+});
